7f594db2c57972c67bc7139c5862756247cde78d
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2023-09-14 09:43:56 +0200

    Initial commit

---
+++ js/kommunedropdown.js
@@ -0,0 +1,58 @@
+console.log("jeg er i dropdown");
+
+const urlKommune = "https://api.dataforsyningen.dk/kommuner";
+
+pbFetchKommuner = document.getElementById("pbFetchKommuner");
+const ddKommuner = document.getElementById("ddKommuner");
+const divTag = document.getElementById("atags")
+
+function fetchAnyUrl(url) {
+    console.log("inside fetch url=" + url)
+    return  fetch(url).then(response => response.json());
+}
+
+function fillDropdownObj(item) {
+    const el = document.createElement("option")
+    console.log(item)
+    el.textContent = item.navn
+    el.value = item //item bliver til en string
+    el.kommune = item
+    ddKommuner.appendChild(el);
+}
+
+function inputChanged(key) {
+    const selindex = ddKommuner.selectedIndex;
+    const selectedOption = ddKommuner.options[selindex];
+    const kommune = selectedOption.value //kan ikke bruges laves op til en string så det virker det ikke så skriver den [Object : object]
+    console.log(kommune)
+    const kom2 = selectedOption.kommune
+    console.log(kom2)
+    createATag(kom2)
+}
+
+function createATag(komObj) {
+    console.log("her kommer obj")
+    console.log(komObj)
+    const aTag = document.createElement("a");
+    console.log(komObj.href)
+    aTag.setAttribute('href', komObj.href)
+    aTag.innerText = komObj.navn;
+    divTag.appendChild(aTag)
+    const brTag = document.createElement('br')
+    divTag.appendChild(brTag);
+}
+
+async function fetchKommuner(urlKommune) {
+    let kommuneArr = await fetchAnyUrl(urlKommune);
+    console.log(kommuneArr);
+    ddKommuner.innerHTML = ""
+    kommuneArr.forEach(fillDropdownObj)
+}
+
+function actionFetch() {
+    const kommuner = fetchKommuner(urlKommune);
+    console.log(kommuner)
+}
+
+pbFetchKommuner.addEventListener('click', inputChanged)
+pbFetchKommuner.addEventListener('click', actionFetch)
